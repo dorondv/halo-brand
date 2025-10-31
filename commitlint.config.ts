8f9e8c353c8172b5fe443e5f12f5ce0b061f5b48
gagishmagi <gagi.shmagi@gmail.com>
2025-11-01 00:28:20 +0200

    chore: update commitlint configuration to allow longer body lines
    - Added a rule to permit up to 200 characters per line in commit message bodies.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,6 +3,9 @@ import type { UserConfig } from '@commitlint/types';
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   ignores: [message => message.startsWith('chore: bump')], // Ignore dependabot commits
+  rules: {
+    'body-max-line-length': [2, 'always', 200], // Allow up to 200 characters per line in body
+  },
 };
 
 export default Configuration;
